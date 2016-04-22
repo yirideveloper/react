c6ac2e93c671af84d00b40d4082182cfe74ccb14
yirideveloper <topyiri1113@gmail.com>
2016-04-22 12:00:06 -0400

    Transpiles the client for a saner including environment.

--- src/server/commands/menus/menuMain.js
+++ src/server/commands/menus/menuMain.js
@@ -5,7 +5,9 @@ const process = (context, action) => {
     name: 'main',
     commands: [
       {key: 'r', name: 'redux', commands: [{type: 'menu.redux'}]},
-      {key: 'h', name: 'help', commands: [{type: 'menu.help'}]},
+      {key: 'c', name: 'clear', commands: []},
+      {key: '-', name: 'score', commands: []},
+      {key: '.', name: 'repeat', commands: []},
       // {key: 'd', name: 'dev menu', commands: [{type: 'menu.devMenu'}]},
       {key: 'q', name: 'quit', commands: [{type: 'program.die'}]}
     ]
